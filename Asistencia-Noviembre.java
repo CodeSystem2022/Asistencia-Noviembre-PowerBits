60748732846417e66853f6d2d1f74405e63a1be9
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2022-11-25 19:34:57 -0300

    AsistenciaNico

--- Asistencia-Noviembre.java
+++ Asistencia-Noviembre.java
@@ -60,3 +60,41 @@ public class Arreglos_Ejercicio_1 {
         }
     } 
 }
+
+// Nico Machuca
+
+/*
+Ejercicio 4: Crear una matriz de tamaño 7x7 y rellenarla de forma que los elementos de la diagonal
+principal sean 1 y el resto 0.
+*/
+package ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][] = new int [7][7];
+        
+        //Llenando la matriz
+        for (int i = 0; i <7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (i==j) {
+                    matriz[i][j] = 1;  
+                }
+                else{
+                    matriz[i][j] = 0;
+                }
+            } 
+        }
+        System.out.println("\nMostrnado matriz completa: ");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }  
+}
