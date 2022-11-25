1f544414616b79b5490478a900cc68a8f64eb8a8
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2022-11-25 18:06:12 -0300

    Asistencia Lucas

--- Asistencia-Noviembre.java
+++ Asistencia-Noviembre.java
@@ -29,3 +29,34 @@ public class Arreglos_Ejercicio_2 {
         }
     }
 }
+
+// Lucas Mendez
+/*
+Ejercicio 1: Leer 5 numeros, guardarlos en un arreglo 
+y mostrarlos en el mismo orden introducidos
+*/
+
+package arreglos_ejercicio_1;
+
+import java.util.Scanner;
+
+public class Arreglos_Ejercicio_1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        // creamos la variable para que el usuario ingrese los numeros
+        int numero;
+        // creamos el arreglo
+        int arreglo[] = new int [5];
+        // creamos el ciclo for para ingresar los datos y guardarlos en el arreglo
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Elemento "+i+"= Digite un numero: ");
+            numero = Integer.parseInt(entrada.nextLine());
+            arreglo[i] = numero;
+        }
+        // creamos el ciclo for para mostrar
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arreglo[i]);
+        }
+    } 
+}
