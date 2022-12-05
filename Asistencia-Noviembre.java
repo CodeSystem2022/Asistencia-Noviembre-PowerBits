9a532ce9acbe3da50efa365074f3f0ab45be92c6
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2022-12-05 10:09:00 -0300

    Asistencia Ivana

--- Asistencia-Noviembre.java
+++ Asistencia-Noviembre.java
@@ -155,3 +155,51 @@ public class Arreglos_Ejercicio_3 {
         System.out.println("\n La cantidad de ceros es: "+conteo0);
     }
 }
+
+//Ivana Gonzalez
+
+/*
+Ejercicio 3: Crear y caragar una matriz de tamaño 3x3, transponerla y mostrarla 
+*/
+package matriz_ejercicio_3;
+
+import java.util.Scanner;
+
+public class Matriz_Ejercicio_3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz [] [] = new int [3] [3];
+        
+        System.out.println("Rellenar la matriz: ");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.println("Matriz ["+i+"]["+j+"]:");
+                matriz[i][j] = entrada.nextInt();
+                
+            }
+        }
+        System.out.println();
+        
+        System.out.println("Matriz original");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.print(matriz[i][j] +" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        
+        //Matriz transpuesta
+        System.out.println("Matriz transpuesta");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.print(matriz[j][i] +" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+}
+
