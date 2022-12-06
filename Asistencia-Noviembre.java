b74fba75634708930177365f6dc78c32b9ec29eb
Héctor Fonzalida <112661891+hector-fonzalida@users.noreply.github.com>
2022-12-06 20:38:28 -0300

    Update Asistencia-Noviembre.java

--- Asistencia-Noviembre.java
+++ Asistencia-Noviembre.java
@@ -202,4 +202,51 @@ public class Matriz_Ejercicio_3 {
     }
     
 }
+// Héctor Fonzalida
+
+/*
+Ejercicio 6: Utilizando 2 matrices de tamaño 5x9 y 9x5, cargar la primera y 
+tramsportarla a la segunda.
+ */
+package matriz_ejercicio_6;
+
+import java.util.Scanner;
+
+public class MatrizEjercicio6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz1[][] = new int[5][9];
+        int matriz2[][] = new int[9][5];
+        
+        System.out.println("Ingrese la matriz: ");
+        for(int i=0; i<5; i++){
+            for(int j=0; j<9; j++){
+                System.out.println("Matriz ["+i+"]["+j+"]: ");
+                matriz1[i][j] = entrada.nextInt();
+            }
+        }
+        System.out.println("\nLa matriz original es: ");
+        for(int i=0; i<5; i++){
+            for(int j=0; j<9; j++){
+                System.out.println(matriz1[i][j]+ " ");
+            }
+            System.out.println("");
+        }
+        
+        // Se traspone
+        for(int i=0; i<5; i++){
+            for(int j=0; j<9; j++){
+                matriz2[j][i] = matriz1[i][j];
+            }
+        }
+        System.out.println("\nLa matriz transpuesta es: ");
+        for(int i=0; i<9; i++){
+            for(int j=0; j<5; j++){
+                System.out.println(matriz2[i][j]+ " "); 
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+}
 
